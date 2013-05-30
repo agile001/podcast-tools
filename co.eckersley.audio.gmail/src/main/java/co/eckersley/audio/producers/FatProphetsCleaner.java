eb24f41bb9f6ae20b90954fdd87e7478b4943461
David Eckersley <david.eckersley@riskflo.com.au>
2013-05-30 11:08:19 +0800

    Text Cleaner tweaks to remove advertising.

--- co.eckersley.audio.gmail/src/main/java/co/eckersley/audio/producers/FatProphetsCleaner.java
+++ co.eckersley.audio.gmail/src/main/java/co/eckersley/audio/producers/FatProphetsCleaner.java
@@ -10,6 +10,10 @@ public class FatProphetsCleaner extends HtmlCleaner {
     private final String FAT_WRAP_MARKER = "BENEFITS OF BECOMING A FAT PROPHETS MEMBER";
     private final String FAT_WEEKLY_MARKER = "Dear Member";
 
+    private final String START_ADVERTISEMENT_MARKER = "--------------------------------------------------------------------------------------------------";
+    private final String END_ADVERTISEMENT_MARKER = "--------------------------------------------------------------------------------------------------";
+    
+
     public String clean(String from, String subject, Document doc) {
 
         if (doc.body().text().contains(FAT_WRAP_MARKER)) {
@@ -37,6 +41,14 @@ public class FatProphetsCleaner extends HtmlCleaner {
                 output.append(e.text()).append(SystemUtils.LINE_SEPARATOR);
             }
         }
+
+        // Strip DailyWealth Adverts
+        int start = output.indexOf(START_ADVERTISEMENT_MARKER);
+        int end = output.indexOf(END_ADVERTISEMENT_MARKER, start + START_ADVERTISEMENT_MARKER.length()) + END_ADVERTISEMENT_MARKER.length() + 1;
+        if (start >= 0 && end >= 0) {
+            output = new StringBuilder(strip(start, end, output.toString()));
+        }
+        
 
         return output.toString();
     }
